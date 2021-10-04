c8e3f42942f440bacec51e45ad591e6fbbbc7b05
Hoda Shobaki <shobkihoda@gmail.com>
2021-10-04 14:55:50 +0300

    Added Algo

--- javaScript/practice1.js
+++ javaScript/practice1.js
@@ -17,3 +17,19 @@ for(let k=1;k<=5;k++){
     sum+=k;
     console.log("Num: "+k+", Sum: "+sum )
 }
+
+// 3-Print each array value and the sum so far
+
+var testArr = [6,3,5,1,2,4];
+var sum=0;
+for(var i=0;i<testArr.length;i++){
+    sum +=testArr[i];
+    console.log("Num: "+testArr[i]+" ,Sum: "+sum)
+}
+
+// 4-Multiply each value in the array by its array position
+var testArr = [6,3,5,1,2,4];
+for(let j=0;j<testArr.length;j++){
+    testArr[j]=testArr[j]*j;
+}
+console.log(testArr);
