ee275d0fb344e3c1ee5c11d6ff75998540de1259
Hoda Shobaki <shobkihoda@gmail.com>
2021-10-20 15:38:20 +0300

    java Hello

---
+++ java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        // Concat using Concat
+      String string1="Hoda";
+      String string2="Mohammad";
+      String string3=string1.concat(string2);
+      System.out.println(string3);
+    //   concating Srtings using + ,"The '+' sign is also overloaded to provide the same functionality of .concat"
+    System.out.println(string1+string2);
+
+    }
+}
+ 
