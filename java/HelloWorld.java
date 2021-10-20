f06cbf84df11a0643bf75c26d17b585259cc5592
Hoda Shobaki <shobkihoda@gmail.com>
2021-10-20 20:50:36 +0300

    built in functions for String

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -11,6 +11,29 @@ public class HelloWorld{
     String newString = String.format(" Hi %s this is your dept %.2f $","Laila", 111.22);
     System.out.println(newString);
     System.out.println(String.format("Hi %s this is your dept %.2f $","laila",111.0));
+
+
+    // indexof
+    String somString="My name is Mohammad Adnan";
+    int a=somString.indexOf("M");
+    System.out.println(a);
+
+    // Trim: The trim() method removes any trailing or leading white spaces from the string.
+    String newString2="  We have the courage to change ourselves  ";
+    System .out.println(newString2.trim());
+
+// Uppercase and Lowercase: The String class provides methods to lowercase and uppercase strings.
+String b="HELLO";
+String c="world";
+System.out.println(b.toLowerCase() + c.toUpperCase());
+
+// Equality: We can compare the equality of a string in two ways. Do they refer to the exact same object,
+//  or do they have the same exact sequence of characters.
+String d="Ahmad";
+String e="Ahmad";
+        System.out.println(d == e);
+        System.out.println(d.equals(e));
     }
+
 }
  
