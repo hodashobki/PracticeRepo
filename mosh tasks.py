64b3dd9564c2ebae7805442411a2aaa3a36b38bf
Hoda Shobaki <shobkihoda@gmail.com>
2021-11-08 23:53:33 +0200

    numbers to words

--- mosh tasks.py
+++ mosh tasks.py
@@ -46,3 +46,22 @@ for item in items:
     if item not in uniqes:
         uniqes.append(item)
 print(uniqes)
+
+# converting numbers to words
+phon_number=input("Enter your Phon Number: ")
+number_mapping={
+    "0":"zero",
+    "1":"one",
+    "2":"two",
+    "3":"three",
+    "4":"four",
+    "5":"five",
+    "6":"six",
+    "7":"seven",
+    "8":"eight",
+    "9":"nine"
+}
+output=""
+for char in phon_number:
+    output += number_mapping.get(char,"!")+ " "
+print(output)
