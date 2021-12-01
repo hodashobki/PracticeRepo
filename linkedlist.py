95816963519a04a9883b22c9719676b0220f10ba
Hoda Shobaki <shobkihoda@gmail.com>
2021-12-01 15:37:43 +0200

    linked list

--- linkedlist.py
+++ linkedlist.py
@@ -5,7 +5,7 @@ class Node:
 class LinkedList:
     def __init__(self) -> None:
         self.head =None
-
+# Add to front
     def addNode(self,val):
         new_node =Node(val)
         if not self.head:
@@ -14,6 +14,21 @@ class LinkedList:
             current = self.head
             new_node.next =current
             self.head =new_node
+
+
+    # Add to the end
+    def addToTheEnd(self,val):
+        if self.head is None:
+            self.addNode(val)
+        else:
+            new_node=Node(val)
+            runner =self.head
+            while runner.next is not None:
+                runner = runner.next
+            runner.next=new_node
+
+
+
     def printList(self):
         if self.head is None:
             print("List is empty")
@@ -29,4 +44,5 @@ llist.addNode(0)
 llist.addNode(2)
 llist.addNode(3)
 llist.addNode(4)
+llist.addToTheEnd(55)
 llist.printList()
